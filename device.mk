44b182f93580ee62fd1e55f4a664167a1a7ee68c
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:21 +0200

    z2_plus: Build Gatekeeper HIDL HAL interface
    Change-Id: I4140f7f17188900d23562e9dfc1e351e1988335f

--- device.mk
+++ device.mk
@@ -169,6 +169,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Gps
 PRODUCT_PACKAGES += \
     gps.msm8996 \
