d8f244b52bd8a534c65fb319e06f21630eb1455c
Michael Bestas <mkbestas@lineageos.org>
2017-10-01 20:28:34 +0300

    z2_plus: Initial overlays
    Change-Id: I3cd61c4945d0ce52d0ed7ccf62ead1fdad8298cf

--- device.mk
+++ device.mk
@@ -17,7 +17,9 @@
 LOCAL_PATH := device/zuk/z2_plus
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
