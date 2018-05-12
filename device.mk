ecd04f6d86567d54191a8b6f2011f644983d40c3
Michael Bestas <mkbestas@lineageos.org>
2018-05-12 23:39:04 +0300

    z2_plus: Update GPS stack to O-MR1
    * ... and kill QC location
    * From bardock 8.1.0 OPM1.171019.019
    
    Change-Id: I237d2a7f2e587a16cde227af4a977b165669894d

--- device.mk
+++ device.mk
@@ -177,17 +177,21 @@ PRODUCT_PACKAGES += \
 # Gps
 PRODUCT_PACKAGES += \
     gps.msm8996 \
+    libgnss \
     libgnsspps \
     libcurl
 
 PRODUCT_PACKAGES += \
-    apdr.conf \
-    flp.conf \
-    gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # Healthd
 PRODUCT_PACKAGES += \
