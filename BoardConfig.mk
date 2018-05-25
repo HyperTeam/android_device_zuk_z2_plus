b9be3c945838ad4f45dc5a07f7b2ca9964a85609
Michael Bestas <mkbestas@lineageos.org>
2018-05-25 19:54:31 +0300

    z2_plus: Add compatibility_matrix.xml
    * Generated with:
      assemble_vintf -m \
      -i system/libhidl/manifest.xml \
      -o device/lenovo/kuntao/compatibility_matrix.xml
    
    * Manual edit:
      Remove comments
      Remove vndk entry
      Set optional="false"
    
    Change-Id: I1886aea25188bbdfd467d5d6764b6fa071216481

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,6 +142,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_z2_plus
