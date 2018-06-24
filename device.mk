6afdb34917feace5d5e1468a5d805e5dcabc8185
karthick111 <karthickrko61@gmail.com>
2018-06-24 10:17:37 +0530

    z2_plus: copy goodix-ts keylayout

--- device.mk
+++ device.mk
@@ -161,6 +161,7 @@ PRODUCT_COPY_FILES += \
 # Keylayouts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/fpc1020tp.kl:system/usr/keylayout/fpc1020tp.kl \
+    $(LOCAL_PATH)/keylayout/goodix-ts.kl:system/usr/keylayout/goodix-ts.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl
 
