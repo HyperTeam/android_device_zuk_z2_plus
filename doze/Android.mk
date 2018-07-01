nibaji/device_zuk_z2_plus

z2_plus: add doze package
Taken from yu lettuce
Includes the following patches

YUDoze: Add service for using proximity sensor for wake-up
Author: Patrick Lower <devvortex@gmail.com>
Date:   Mon May 11 16:14:17 2015 -0400

YUDoze: Set gesture to disabled by default
Author: Arne Coucheron <arco68@gmail.com>
Date:   Wed May 20 00:56:19 2015 +0200

YUDoze: Consolidate doze settings
Author: Christopher R. Palmer <crpalmer@gmail.com>
Date:   Sat May 23 11:42:49 2015 -0700

YUDoze: fix defaults
Author: Dave Daynard <nardholio@gmail.com>
Date:   Sat May 23 18:29:13 2015 -0400

YUDoze: Add back button to action bar
Author: luca020400 <luca.stefani.ge1@gmail.com>
Date:   Sun May 24 15:47:17 2015 -0700

YUDoze: add proximity wake
Author: Dave Daynard <nardholio@gmail.com>
Date:   Mon May 25 12:54:00 2015 -0400

YUDoze: restore proximity wake preference on boot
Author: Dave Daynard <nardholio@gmail.com>
Date:   Sat Jul 4 13:44:53 2015 -0400

YUDozeService: Prevent a pm wakeup with a timestamp in the future
Author: Brandon McAnsh <brandon.mcansh@gmail.com>
Date:   Fri Jul 24 15:40:45 2015 -0400

YUDozeService: properly fix future timestamp error
Author: Patrick Lower <devvortex@gmail.com>
Date:   Wed Aug 5 10:51:03 2015 -0400

YUDoze: fix typo
Author: Dave Daynard <nardholio@gmail.com>
Date:   Tue Sep 8 22:56:10 2015 -0400

YUDoze: Use ScreenType util from cmsdk
Author: Michael Bestas <mikeioannina@gmail.com>
Date:   Sun Dec 13 02:51:09 2015 -0500

YUDOze: Remove summary for YUDoze
Author: TheStrix <parthbhatia98@gmail.com>
Date:   Sun Dec 13 07:25:36 2015 -0500

Change-Id: Ia7e668622eecf4355fd55dc9cda7ca05a6494b93
Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

doze/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := ZukDoze
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.internal
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))

doze/src/com/cyanogenmod/settings/doze/BootCompletedReceiver.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "ZukDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startService(new Intent(context, ZukDozeService.class));
+    }
+
+}

HyperTeam/android_device_zuk_z2_plus

doze/Android.mk
@@ -1,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := XiaomiDoze
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.cyanogenmod.platform.internal
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))

doze/src/com/cyanogenmod/settings/doze/BootCompletedReceiver.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.doze;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiDoze";
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startService(new Intent(context, XiaomiDozeService.class));
+    }
+
+}