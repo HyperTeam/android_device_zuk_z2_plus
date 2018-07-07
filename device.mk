aff56192752a18971e2be81328c04a8dc243ad94
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    z2_plus: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Ifb39b7c2c8de2cbb1f59c6dc492a3763d32f1b7a

--- device.mk
+++ device.mk
@@ -124,9 +124,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
-    Snap \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    Snap
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/msm8996_camera.xml:system/etc/camera/msm8996_camera.xml \
