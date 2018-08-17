ed96037f40dc10f49740723a5bf82fb6e6a929f7
Bruno Martins <bgcngm@gmail.com>
2018-08-17 07:37:38 +0200

    z2_plus: Use QTI bluetooth stack
    Signed-off-by: harry8242 <mohamedharis97@gmail.com>

--- device.mk
+++ device.mk
@@ -121,10 +121,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libbt-vendor
 
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
